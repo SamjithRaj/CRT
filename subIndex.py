00016b49cc7813b4d2e5d958d1cd59e1e0f50465
Samjith <144241221+SamjithRaj@users.noreply.github.com>
2025-10-28 12:33:57 +0530

    Add files via upload

---
+++ subIndex.py
@@ -0,0 +1,16 @@
+gList = []
+
+def index(s, t, i=0):
+    if(i == len(s)):
+        gList.append(t)
+        return
+    op1 = t+s[i]
+    op2 = t
+    index(s, op1, i+1)
+    index(s, op2, i+1)
+    return
+
+s = 'abc'
+t = ''
+index(s,t)
+print(gList)
