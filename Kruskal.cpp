a0c5840e9fdede1220f02691ae66670c65a277bc
Samjith <reachsamjith@gmail.com>
2026-01-05 12:56:47 +0530

    Update Kruskal.cpp

--- Kruskal.cpp
+++ Kruskal.cpp
@@ -2,38 +2,56 @@
 using namespace std;
 
 
-int minKey(vector<int> &key, vector<bool> &mstSet) {
-    int min = INT_MAX, min_index;
+struct Edge {
+    int u, v, weight;
+    bool operator<(const Edge& other) const {
+        return weight < other.weight;
+    }
+};
 
-    for (int v = 0; v < mstSet.size(); v++)
-        if (mstSet[v] == false && key[v] < min)
-            min = key[v], min_index = v;
+int find(int i, vector<int> &parent) {
+    if (parent[i] == i)
+        return i;
+    return parent[i] = find(parent[i], parent);
+}
 
-    return min_index;
+void union(int u, int v, vector<int> &parent, vector<int> &ranks) {
+    int root_u = find(u, parent);
+    int root_v = find(v, parent);
+    if (root_u != root_v) {
+        if (ranks[root_u] < ranks[root_v])
+            parent[root_u] = root_v;
+        else if (ranks[root_u] > ranks[root_v])
+            parent[root_v] = root_u;
+        else {
+            parent[root_v] = root_u;
+            ranks[root_u]++;
+        }
+    }
 }
 
 int KruskalMST(vector<vector<int>> &graph) {
     int V = graph.size();
-    vector<int> parent(V);
-    vector<int> key(V);
-    vector<bool> mstSet(V); 
+    vector<Edge> edges;
     for (int i = 0; i < V; i++)
-        key[i] = INT_MAX, mstSet[i] = false;
+        for (int j = i + 1; j < V; j++)
+            if (graph[i][j] != 0)
+                edges.push_back({i, j, graph[i][j]});
 
-    key[0] = 0;
-    parent[0] = -1;
-    for (int count = 0; count < V - 1; count++) {
-        int u = minKey(key, mstSet);
-        mstSet[u] = true;
-        for (int v = 0; v < V; v++)
-            if (graph[u][v] && mstSet[v] == false && graph[u][v] < key[v])
-                parent[v] = u, key[v] = graph[u][v];
-                int totalWeight = 0;
-    for (int i = 1; i < graph.size(); i++)
-        totalWeight += graph[parent[i]][i];
+    sort(edges.begin(), edges.end());
+
+    vector<int> parent(V), ranks(V, 0);
+    for (int i = 0; i < V; i++) parent[i] = i;
+
+    int totalWeight = 0;
+    for (const auto& edge : edges) {
+        if (find(edge.u, parent) != find(edge.v, parent)) {
+            totalWeight += edge.weight;
+            union(edge.u, edge.v, parent, ranks);
+        }
+    }
     cout << "Total Minimum Weight: " << totalWeight << endl;
     return 0;
-    }   
 }
 
 int main() {
