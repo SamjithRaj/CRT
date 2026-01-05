67ba01dd035f3178b6f42960b89b713d3e57f832
Samjith <reachsamjith@gmail.com>
2026-01-05 10:14:22 +0530

    Update treasureGraph.cpp

--- treasureGraph.cpp
+++ treasureGraph.cpp
@@ -2,50 +2,55 @@
 using namespace std;
 
 int main(){
-    int n, m;
-    cin >> n >> m;
-    vector<vector<int>> a(n, vector<int>(m));
-    int sx, sy;
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            cin >> a[i][j];
-            if(a[i][j] == 2){
-                sx = i;
-                sy = j;
+    int rows, cols;
+    cin >> rows >> cols;
+    vector<vector<int>> grid(rows, vector<int>(cols));
+    int startX, startY;
+    for(int i = 0; i < rows; i++){
+        for(int j = 0; j < cols; j++){
+            cin >> grid[i][j];
+            if(grid[i][j] == 2){
+                startX = i;
+                startY = j;
             }
         }
     }
 
     queue<pair<int, int>> q;
-    q.push({sx, sy});
+    q.push({startX, startY});
     
-    vector<vector<int>> d(n, vector<int>(m, -1));
-    d[sx][sy] = 0;
-
-    int dx[] = {-1, 1, 0, 0};
-    int dy[] = {0, 0, -1, 1};
+    vector<vector<int>> distanceMatrix(rows, vector<int>(cols, -1));
+    distanceMatrix[startX][startY] = 0;
 
     while(!q.empty()){
-        pair<int, int> p = q.front();
+        pair<int, int> currentCell = q.front();
         q.pop();
-        int x = p.first;
-        int y = p.second;
+        int currRow = currentCell.first;
+        int currCol = currentCell.second;
 
-        if(a[x][y] == 3){
-            cout << "Shortest Path: " << d[x][y] << endl;
+        if(grid[currRow][currCol] == 3){
+            cout << "Shortest Path: " << distanceMatrix[currRow][currCol] << endl;
             return 0;
         }
 
-        for(int i = 0; i < 4; i++){
-            int nx = x + dx[i];
-            int ny = y + dy[i];
 
-            if(nx >= 0 && nx < n && ny >= 0 && ny < m){
-                if(d[nx][ny] == -1 && (a[nx][ny] == 1 || a[nx][ny] == 3)){
-                    d[nx][ny] = d[x][y] + 1;
-                    q.push({nx, ny});
-                }
-            }
+        if(currRow - 1 >= 0 && distanceMatrix[currRow - 1][currCol] == -1 && (grid[currRow - 1][currCol] == 1 || grid[currRow - 1][currCol] == 3)){
+            distanceMatrix[currRow - 1][currCol] = distanceMatrix[currRow][currCol] + 1;
+            q.push({currRow - 1, currCol});
+        }
+
+        if(currRow + 1 < rows && distanceMatrix[currRow + 1][currCol] == -1 && (grid[currRow + 1][currCol] == 1 || grid[currRow + 1][currCol] == 3)){
+            distanceMatrix[currRow + 1][currCol] = distanceMatrix[currRow][currCol] + 1;
+            q.push({currRow + 1, currCol});
+        }
+
+        if(currCol - 1 >= 0 && distanceMatrix[currRow][currCol - 1] == -1 && (grid[currRow][currCol - 1] == 1 || grid[currRow][currCol - 1] == 3)){
+            distanceMatrix[currRow][currCol - 1] = distanceMatrix[currRow][currCol] + 1;
+            q.push({currRow, currCol - 1});
+        }
+        if(currCol + 1 < cols && distanceMatrix[currRow][currCol + 1] == -1 && (grid[currRow][currCol + 1] == 1 || grid[currRow][currCol + 1] == 3)){
+            distanceMatrix[currRow][currCol + 1] = distanceMatrix[currRow][currCol] + 1;
+            q.push({currRow, currCol + 1});
         }
     }
     cout << "No Path Found" << endl;
