3ba316d13e03b398909d2d842f8cc17084234503
Samjith <reachsamjith@gmail.com>
2026-01-07 09:19:03 +0530

    Update fibonacci.cpp

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -4,14 +4,20 @@ using namespace std;
 int main(){
     int n; 
     cin >> n; 
-    vector<int> fib(n);
+    if(n <= 0) return 0;
+    vector<long long> fib(n);
     fib[0] = 0;
-    if(n > 1) fib[1] = 1;
+    long long sum = 0;
+    sum += fib[0];
+    if(n > 1) {
+        fib[1] = 1;
+        sum += fib[1];
+    }
     for(int i = 2; i<n; i++){
         fib[i] = fib[i-1] + fib[i-2];
+        sum += fib[i];
     }
-    for(int i = 0; i<n; i++){
-        cout << fib[i] << " ";
-    }
-    cout << endl;
+    cout << "Nth Fibonacci Term: " << fib[n-1] << endl;
+    cout << "Sum of " << n << " terms: " << sum << endl;
+    return 0;
 }
