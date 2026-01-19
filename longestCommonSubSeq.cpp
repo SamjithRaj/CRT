2e06213767f9add84061ce7120519a4210e94cf9
Samjith <reachsamjith@gmail.com>
2026-01-19 13:17:03 +0530

    Create longestCommonSubSeq.cpp

---
+++ longestCommonSubSeq.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void longestCommonSubSeq(string s1, string s2) {
+    int n = s1.length();
+    int m = s2.length();
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+    for(int i = 1; i <= n; i++){
+        for(int j = 1; j <= m; j++){
+            if(s1[i - 1] == s2[j - 1]){
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            } else {
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            }
+        }
+    }
+
+    string lcs = "";
+    int i = n, j = m;
+    while(i > 0 && j > 0){
+        if(s1[i-1] == s2[j-1]){
+            lcs += s1[i-1];
+            i--;
+            j--;
+        } else if(dp[i-1][j] > dp[i][j-1]){
+            i--;
+        } else {
+            j--;
+        }
+    }
+    reverse(lcs.begin(), lcs.end());
+    cout << lcs << endl;
+}
+
+int main(){
+    string s1, s2; 
+    cin >> s1 >> s2;
+    longestCommonSubSeq(s1, s2);
+    return 0;
+}
