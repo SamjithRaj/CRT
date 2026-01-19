6b37f8c26dabee7d8729b7760831a29228d9ef1f
Samjith <reachsamjith@gmail.com>
2026-01-19 11:07:32 +0530

    Update 01Knapsack.cpp

--- 01Knapsack.cpp
+++ 01Knapsack.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 struct item {
-    int value;
+    int profit;
     int weight;
 };
 
 bool compare(item a, item b) {
-    double r1 = (double)a.value / a.weight;
-    double r2 = (double)b.value / b.weight;
+    double r1 = (double)a.profit / a.weight;
+    double r2 = (double)b.profit / b.weight;
     return r1 > r2;
 }
 
@@ -17,7 +17,7 @@ void knapsack01(int W) {
     cin >> n; 
     item arr[n];
     for(int i = 0; i < n; i++){
-        cin >> arr[i].value >>  arr[i].weight;
+        cin >> arr[i].profit >>  arr[i].weight;
     }
     sort(arr, arr + n, compare);
 
@@ -28,7 +28,7 @@ void knapsack01(int W) {
             if(i == 0 || w == 0){
                 dp[i][w] = 0;
             } else if(arr[i - 1].weight <= w){
-                dp[i][w] = max(arr[i - 1].value + dp[i - 1][w - arr[i - 1].weight], dp[i - 1][w]);
+                dp[i][w] = max(arr[i - 1].profit + dp[i - 1][w - arr[i - 1].weight], dp[i - 1][w]);
             } else {
                 dp[i][w] = dp[i - 1][w];
             }
