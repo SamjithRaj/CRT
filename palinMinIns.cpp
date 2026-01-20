e03187d7bf9bdf58ce49dd19adc3168b5fdaeb52
Samjith <reachsamjith@gmail.com>
2026-01-20 11:21:07 +0530

    Update palinMinIns.cpp

---
+++ palinMinIns.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int longestPalindromicSubsequence(string s) {
+    int n = s.length();
+    if (n == 0) return 0;
+    vector<vector<int>> dp(n, vector<int>(n));
+
+    for (int i = 0; i < n; i++)
+        dp[i][i] = 1;
+
+    for (int cl = 2; cl <= n; cl++) {
+        for (int i = 0; i < n - cl + 1; i++) {
+            int j = i + cl - 1;
+            if (s[i] == s[j] && cl == 2)
+                dp[i][j] = 2;
+            else if (s[i] == s[j])
+                dp[i][j] = dp[i + 1][j - 1] + 2;
+            else
+                dp[i][j] = max(dp[i][j - 1], dp[i + 1][j]);
+        }
+    }
+    return dp[0][n - 1];
+}
+
+int main(){
+    string s;
+    cin >> s;
+    cout << s.length() - longestPalindromicSubsequence(s) << endl;
+    return 0;
+}
